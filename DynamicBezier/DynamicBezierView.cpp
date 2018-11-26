caec575b344ec9e3b831564da4c9d43aebba058b
Heliovic <1005031096@qq.com>
2018-11-26 18:39:00 +0800

    Dynamic Bezier: New project

---
+++ DynamicBezier/DynamicBezierView.cpp
@@ -0,0 +1,104 @@
+﻿
+// DynamicBezierView.cpp: CDynamicBezierView 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "DynamicBezier.h"
+#endif
+
+#include "DynamicBezierDoc.h"
+#include "DynamicBezierView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CDynamicBezierView
+
+IMPLEMENT_DYNCREATE(CDynamicBezierView, CView)
+
+BEGIN_MESSAGE_MAP(CDynamicBezierView, CView)
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+END_MESSAGE_MAP()
+
+// CDynamicBezierView 构造/析构
+
+CDynamicBezierView::CDynamicBezierView() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CDynamicBezierView::~CDynamicBezierView()
+{
+}
+
+BOOL CDynamicBezierView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CDynamicBezierView 绘图
+
+void CDynamicBezierView::OnDraw(CDC* /*pDC*/)
+{
+	CDynamicBezierDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CDynamicBezierView 打印
+
+BOOL CDynamicBezierView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void CDynamicBezierView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加额外的打印前进行的初始化过程
+}
+
+void CDynamicBezierView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加打印后进行的清理过程
+}
+
+
+// CDynamicBezierView 诊断
+
+#ifdef _DEBUG
+void CDynamicBezierView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CDynamicBezierView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CDynamicBezierDoc* CDynamicBezierView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CDynamicBezierDoc)));
+	return (CDynamicBezierDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CDynamicBezierView 消息处理程序
---
+++ DynamicBezier/DynamicBezierView.h
@@ -0,0 +1,49 @@
+﻿
+// DynamicBezierView.h: CDynamicBezierView 类的接口
+//
+
+#pragma once
+
+
+class CDynamicBezierView : public CView
+{
+protected: // 仅从序列化创建
+	CDynamicBezierView() noexcept;
+	DECLARE_DYNCREATE(CDynamicBezierView)
+
+// 特性
+public:
+	CDynamicBezierDoc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 实现
+public:
+	virtual ~CDynamicBezierView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // DynamicBezierView.cpp 中的调试版本
+inline CDynamicBezierDoc* CDynamicBezierView::GetDocument() const
+   { return reinterpret_cast<CDynamicBezierDoc*>(m_pDocument); }
+#endif
+
